bc02c5542db550296ed576556de75d3fc94485ad
Lyndsey Browning <lbrowning86@gmail.com>
2017-02-13 20:12:26 +0000

    Removed unneeded default parameter

--- src/checkPrefix.js
+++ src/checkPrefix.js
@@ -1,6 +1,6 @@
 import utils from './utils';
 
-export default function contains(node = {}, prefix) {
+export default function contains(node, prefix) {
   const input = prefix.toLowerCase().split('');
   const found = input.every((letter, index) => {
     if(!node[letter]) {
