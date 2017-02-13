0e860cd048f9e008fe9b782f357e38c58a9db3d7
Lyndsey Browning <lbrowning86@gmail.com>
2017-02-13 20:08:05 +0000

    Test getWords after adding and removing from Trie

--- __tests__/getWords.js
+++ __tests__/getWords.js
@@ -8,4 +8,13 @@ describe('Retrieving a full list of words in the trie', () => {
   it('retrieves the full list of words in the trie sorted alphabetically', () => {
     expect(actual).toEqual(expected);
   });
+
+  test('adding new words', () => {
+    const input = ['one',  'two', 'three'];
+    const data = trie(input).addWord('four').removeWord('one');
+    const actual = data.getWords();
+    const expected = ['four', 'three', 'two'];
+
+    expect(actual).toEqual(expected);
+  });
 });
