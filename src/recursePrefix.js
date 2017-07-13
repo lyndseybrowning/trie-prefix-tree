e1cd6e31fc9419d5a302bc00a20225b34a0eb0da
Lyndsey Browning <lbrowning86@gmail.com>
2017-07-13 20:39:30 +0100

    fix: Replace default .sort() mechanism in recursePrefix
    The default .sort() method produces poor performance in a NodeJs environment. The recursePrefix
    method has been refactored to sort items as they are being added versus when all items have been
    found.

--- src/recursePrefix.js
+++ src/recursePrefix.js
@@ -1,15 +1,32 @@
 import config from './config';
 
+// sort items as they're being found
+// to prevent slow .sort() in NodeJs
+const pushInOrder = function(word, prefixes) {
+  let i = 0;
+
+  while(i < prefixes.length) {
+    if(word < prefixes[i]) {
+      break;
+    }
+    i += 1;
+  }
+
+  prefixes.splice(i, 0, word);
+
+  return prefixes;
+};
+
 export default function recursePrefix(node, prefix, prefixes = []) {
   let word = prefix;
 
   for(const branch in node) {
     if(branch === config.END_WORD) {
-      prefixes.push(word);
+      pushInOrder(word, prefixes);
       word = '';
     }
     recursePrefix(node[branch], prefix + branch, prefixes);
   }
 
-  return prefixes.sort();
+  return prefixes;
 };
