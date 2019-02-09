9fd037d5e2ef3627beb59d2e8d60323bd2220eb5
Lyndsey Browning <lbrowning86@gmail.com>
2019-02-09 19:44:04 +0000

    fix: Fixes #8.
    Allow words containing the end word character to be added to the trie.
    
    #8

--- src/append.js
+++ src/append.js
@@ -1,13 +1,21 @@
 import config from './config';
-import utils from './utils';
 
 export default function append(trie, letter, index, array) {
-  trie[letter] = trie[letter] || {};
-  trie = trie[letter];
+  const isEndWordLetter = letter === config.END_WORD;
+  const isLastLetter = index === array.length - 1;
 
-  if(index === array.length - 1) {
+  if(isEndWordLetter && !isLastLetter) {
+    trie[config.END_WORD] = 1;
+    trie[config.END_WORD_REPLACER] = {};
+    trie = trie[config.END_WORD_REPLACER];
+  } else {
+    trie[letter] = trie[letter] || {};
+    trie = trie[letter];
+  }
+
+  if(isLastLetter) {
     trie[config.END_WORD] = 1;
   }
 
   return trie;
-};
+}
--- src/recursePrefix.js
+++ src/recursePrefix.js
@@ -21,16 +21,19 @@ export default function recursePrefix(node, prefix, sorted, prefixes = []) {
   let word = prefix;
 
   for(const branch in node) {
-    if(branch === config.END_WORD) {
+    let currentLetter = branch;
+    if(branch === config.END_WORD && typeof node[branch] === 'number') {
       if(sorted) {
         pushInOrder(word, prefixes);
       } else {
         prefixes.push(word);
       }
       word = '';
+    } else if(branch === config.END_WORD_REPLACER) {
+      currentLetter = config.END_WORD;
     }
-    recursePrefix(node[branch], prefix + branch, sorted, prefixes);
+    recursePrefix(node[branch], prefix + currentLetter, sorted, prefixes);
   }
 
   return prefixes;
-};
+}
